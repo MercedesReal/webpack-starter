91ce49e2a5d6be404590c9323b36fc2164a7c1b1
Michelle] <mishigalarza21@gmail.com>
2021-12-31 16:19:41 -0400

    Primer Comit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-
-import {saludar} from './js/componentes';
 import './styles.css'
 
-const nombre = 'Michelle'
+import { Todo, TodoList, crearTodoHtml } from "./classes";
 
-saludar(nombre);
+export const todoList = new TodoList();
+
+//todoList.todos.forEach(todo => crearTodoHtml( todo ));
+todoList.todos.forEach( crearTodoHtml );
+
+console.log('todos', todoList.todos);
+
+
+
+
+
+
+
+
+
+
+
+
+// const tarea = new Todo('Aprender JavaScript');
+// todoList.nuevoTodo( tarea );
+// console.log( todoList );
+// crearTodoHtml( tarea );
+
+// localStorage.setItem('mi-key', 'ABC1234');
+// sessionStorage.setItem('mi-key', 'ABC1234');
+
+// setTimeout(() => {
+//   localStorage.removeItem('mi-key');
+// }, 1500);
